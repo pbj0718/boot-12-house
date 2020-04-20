eb427b6c6499bcba7711961dc1ff9db5f489943d
panbojin <643849802@qq.com>
2020-04-20 17:53:37 +0800

    地区查询整合swagger2

--- src/main/java/com/etoak/controller/AreaController.java
+++ src/main/java/com/etoak/controller/AreaController.java
@@ -2,6 +2,7 @@ package com.etoak.controller;
 
 import com.etoak.bean.Area;
 import com.etoak.service.AreaService;
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/area")
 @Slf4j
+@Api(tags = "地区查询")
 public class AreaController {
 
     @Autowired
@@ -25,6 +27,19 @@ public class AreaController {
      * @return
      */
     @GetMapping("/queryByPid")
+    @ApiOperation(value = "根据父id查询子级地区列表",notes = "根据父id查询子级地区列表")
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "接口不存在"),
+            @ApiResponse(code = 401, message = "无权限访问"),
+            @ApiResponse(code = 403, message = "禁止访问"),
+            @ApiResponse(code = 200, message = "成功"),
+            @ApiResponse(code = 500, message = "服务端错误")
+    })
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "id", value="父id",required = false,
+                    defaultValue = "0",paramType = "query"
+            )
+    })
     public List<Area> queryByPid(@RequestParam(required = false,defaultValue = "0")int pid){
         log.info("pid -{}",pid);
         return areaService.queryByPid(pid);
